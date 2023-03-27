de017dd9c363dfea9d851223dabe71350e389b6f
paulhabeeb <paulhabeeb@hey.com>
2023-03-27 15:50:16 -0400

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang='en'>
             <Head>
                 <link rel='icon' href='/favicon.ico' sizes='any' />
-                <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+                <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
 
                 <link
                     rel='stylesheet'
